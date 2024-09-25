2ef3aae07e59bfe60e70e2d958e1cb79cc0ae09a
User <User@LAB2-8>
2024-09-25 10:32:27 +0300

    hgf

--- ToiletFinderServer/DataBaseCreation.sql
+++ ToiletFinderServer/DataBaseCreation.sql
@@ -12,7 +12,7 @@ Go
 
 -- יצירת טבלת סוגי משתמשים
 CREATE TABLE UTypes (
-    UserType INT PRIMARY KEY,                 -- מפתח ראשי
+    UserType INT IDENTITY(1,1) PRIMARY KEY,                 -- מפתח ראשי
     TypeName NVARCHAR(100)                    -- שם של הסוג
 )
 
@@ -24,17 +24,17 @@ CREATE TABLE Users (
     Email NVARCHAR(100),                       -- מייל
     PhoneNumber NVARCHAR(100),                 -- מספר טלפון
     DateOfBirth DATE ,                         -- תאריך לידה של המשתמש
-    UserType INT,                              -- מפתח משני, סוג משתמש 
+    UserType INT                               -- מפתח משני, סוג משתמש 
       FOREIGN KEY (UserType) REFERENCES UTypes(UserType)   -- קישור לטבלת הסוגים
 )
 
 -- יצירת טבלת שירותים קיימים  
 CREATE TABLE CurrentToilets (
     ToiletId INT PRIMARY KEY identity,        -- מפתח ראשי
-    TLocation NVARCHAR(100),                  -- שם משתמש
-    Accessibility NVARCHAR(100),              -- סיסמא
-    Price FLOAT,                              -- מייל
-    ToiletPictures VARBINARY(MAX)             -- תמונות
+    TLocation NVARCHAR(100),                  -- מיקום
+    Accessibility BIT,                        -- נגישות
+    Price FLOAT,                              -- מחיר
+    --ToiletPictures VARBINARY(MAX)             -- תמונות
 )
 
 -- יצירת טבלת דירוגים
@@ -46,7 +46,26 @@ CREATE TABLE Rates (
 
 -- יצירת טבלת ביקורות
 CREATE TABLE Reviews (
-    Review NVARCHAR,                           -- הביקורת
+    Review NVARCHAR(250),                           -- הביקורת
     ToiletID INT PRIMARY KEY,                  -- מפתח משני וראשי מזהה שירותים
     FOREIGN KEY (ToiletId) REFERENCES CurrentToilets(ToiletId),  -- קישור לטבלת השירותים
 )
+
+INSERT INTO UTypes Values('General')
+INSERT INTO UTypes Values('Service Provider')
+INSERT INTO UTypes Values('Sanitaion Maneger')
+INSERT INTO UTypes Values('Admin')
+
+INSERT INTO Users Values('ShaharOz', 'ShaharOz1', 'shahar.oz@gmail.com', '054-5264648', '2007/08/29', 4)
+INSERT INTO Users Values('ShaharShalgi', 'Fluffy234', 'shahar.shalgi@gmail.com', '052-5381648', '2007/02/26', 1)
+
+INSERT INTO CurrentToilets Values('Baker Street 2, London', 0, 2)
+
+INSERT INTO Rates Values(3, 1)
+
+INSERT INTO Reviews Values('Fine. Not great.', 1)
+
+SELECT * FROM CurrentToilets
+
+
+
