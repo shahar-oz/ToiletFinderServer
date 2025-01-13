da1c12b56fb163c6e50c0dcc92ab1bff3f0d10fc
User <User@LAB3-3>
2025-01-13 15:20:08 +0200

    g

--- ToiletFinderServer/DTO/RateDTO.cs
+++ ToiletFinderServer/DTO/RateDTO.cs
@@ -8,11 +8,11 @@ namespace ToiletFinderServer.DTO
         public int ToiletId {  get; set; }
         public virtual CurrentToilet Toilet { get; set; } = null!;
 
-        //public RateDTO() { }
-        //public RateDTO(Models.Rate modelUser)
-        //{
-        //    this.Rate1 = modelUser.Rate1;
-        //    this.ToiletId = modelUser.ToiletId
-        //}
+        public RateDTO() { }
+        public RateDTO(Models.Rate modelUser)
+        {
+            this.Rate1 = modelUser.Rate1;
+            this.ToiletId = modelUser.ToiletId;
+        }
     }
 }
